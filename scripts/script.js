3a1665d5481f925fc7e4d714d788a071ebddf85c
Kamal Mustafayev <temple23062@gmail.com>
2024-04-26 00:16:29 +0400

    Made the whole project

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const fromInput = document.getElementById('input-from');
 const resultInput = document.getElementById('input-to');
-
+const buttonsFrom = document.querySelectorAll('#currency-from .currency-buttons button'); 
+const buttonsResult = document.querySelectorAll('#currency-to .currency-buttons button');
 
 const getCurrencyData = async () => {
     const buttonSelectedFrom = document.querySelector("#currency-from .selected-button").textContent;
@@ -12,66 +13,41 @@ const getCurrencyData = async () => {
                 item.textContent = `1 ${buttonSelectedFrom} = 1 ${buttonSelectedTo}`;
             })
     }else{
-        const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${buttonSelectedFrom.toLowerCase()}.min.json`);
-        const data = await response.json();
-        resultInput.value = Number(fromInput.value) * data[`${buttonSelectedFrom.toLowerCase()}`][`${buttonSelectedTo.toLowerCase()}`]
+        const response1 = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${buttonSelectedFrom.toLowerCase()}.min.json`);
+        const data1 = await response1.json();
+        resultInput.value = Number(fromInput.value) * data1[`${buttonSelectedFrom.toLowerCase()}`][`${buttonSelectedTo.toLowerCase()}`]
         const currencyDescriptionFrom = document.getElementById('currency-description-from')
-        currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data[buttonSelectedTo.toLowerCase()]} ${buttonSelectedTo}`;
-        getInverseCurrencyData();
+        currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data1[`${buttonSelectedFrom.toLowerCase()}`][buttonSelectedTo.toLowerCase()]} ${buttonSelectedTo}` ;
+        const response2 = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${buttonSelectedTo.toLowerCase()}.min.json`);
+        const data2 = await response2.json();
+        const currencyDescriptionTo = document.getElementById('currency-description-to')
+        currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data2[`${buttonSelectedTo.toLowerCase()}`][buttonSelectedFrom.toLowerCase()]} ${buttonSelectedFrom}` ;
     }  
-}
-
-const getInverseCurrencyData = async () => {
-    const buttonSelectedFrom=document.querySelector('#currency-from .selected-button').textContent;
-    const buttonSelectedTo = document.querySelector('#currency-to .selected-button').textContent;
-    const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${buttonSelectedTo.toLowerCase()}.min.json`);
-    const data = await response.json();
-    const currencyDescriptionTo = document.getElementById('currency-description-to')
-    currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data[`${buttonSelectedTo.toLowerCase()}`][buttonSelectedFrom.toLowerCase()]} ${buttonSelectedFrom}` 
 }
 
 function currencyHandler(){
     getCurrencyData()
     .catch(error => {
-        console.log(error)
+        alert(`${error}! Something went wrong!`);
     })
 }
 
-let [flag1, flag2] = [true, true];  //FLAGS
-const buttonsFrom = document.querySelectorAll('#currency-from button');  //FROM BUTTONS WORK
-
-buttonsFrom.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (flag1 === true) {
-            buttonsFrom.forEach((item) => {
-                item.classList.remove("selected-button")
-            })
-            item.classList.add("selected-button")
-            flag1 = false;
-        }else {
-            item.classList.remove("selected-button")
-            flag1 = true;
-        }
-        currencyHandler();
+buttonsFrom.forEach((item) =>{
+    item.addEventListener('click', function() {
+       buttonsFrom.forEach((item) =>{
+                       item.classList.remove('selected-button');
+                       this.classList.add('selected-button');
+                   });
+                   currencyHandler();
     })
-})
-
-const buttonsResult = document.querySelectorAll('#currency-to .currency-buttons button')  //TO BUTTONS WORK
-
-buttonsResult.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (flag2 === true) {
-            buttonsResult.forEach((item) => {
-                item.classList.remove("selected-button")
-            })
-            item.classList.add("selected-button")
-            flag2 = false
-        } else {
-            item.classList.remove("selected-button")
-            flag2 = true
-        }
-        currencyHandler();
+});
+buttonsResult.forEach((item) =>{
+    item.addEventListener('click', function() {
+       buttonsResult.forEach((item) =>{
+                       item.classList.remove('selected-button');
+                       this.classList.add('selected-button');
+                   });
+                   currencyHandler();
     })
-})
-
+});
 fromInput.addEventListener('keyup',currencyHandler);
